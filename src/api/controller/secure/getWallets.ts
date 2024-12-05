0cba44d79ebd305425a650d8132f897a387a8685
ElSierra <hojoisaac@gmail.com>
2024-12-05 19:01:55 +0100

    refactor: format getWallets function for improved readability and maintainability

--- src/api/controller/secure/getWallets.ts
+++ src/api/controller/secure/getWallets.ts
@@ -3,18 +3,20 @@ import { sendResponse } from "../../../util/sendResponse";
 import type { Request, Response } from "express";
 import knex from "../../../../db/knex";
 export const getWallets = async (req: Request, res: Response) => {
-  const wallets = await knex("wallets").select({
-    id: "id", // Rename the "id" column to "walletId"
-    emoji:"emojiId",
-    walletName: "name",
-    walletBalance: "balance",
-    walletAddress: "address", 
-    walletBalanceUSD: "usd_balance", 
-    // Rename the "balance" column to "walletBalance"
-    createdAt: "created_at", 
-  }).where({
-    user_id: req.user?.id,
-  });
+  const wallets = await knex("wallets")
+    .select({
+      id: "id", // Rename the "id" column to "walletId"
+      emoji: "emojiId",
+      walletName: "name",
+      walletBalance: "balance",
+      walletAddress: "address",
+      walletBalanceUSD: "usd_balance",
+      // Rename the "balance" column to "walletBalance"
+      createdAt: "created_at",
+    })
+    .where({
+      user_id: req.user?.id,
+    });
   const balance = await knex("balance")
     .select("total_balance", "total_balance_usd")
     .where({ user_id: req.user?.id })
@@ -23,8 +25,14 @@ export const getWallets = async (req: Request, res: Response) => {
 
   console.log("🚀 ~ file: getWallets.ts:15 ~ getWallets ~ balance:", balance);
   const walletWithBalance = {
-    adjustSOL: balance[0].total_balance ||0 >= balance[1].total_balance||0 ? "up" : "down",
-    adjustUSD: balance[0].total_balance_usd || 0 >= balance[1].total_balance_usd || 0 ? "up" : "down",
+    adjustSOL:
+      (balance[0].total_balance || 0) >= (balance[1].total_balance || 0)
+        ? "up"
+        : "down",
+    adjustUSD:
+      (balance[0].total_balance_usd || 0) >= (balance[1].total_balance_usd || 0)
+        ? "up"
+        : "down",
     prevBalance: balance[1].total_balance || 0,
     prevBalanceUSD: balance[1].total_balance_usd || 0,
     currentBalance: balance[0].total_balance || 0,
